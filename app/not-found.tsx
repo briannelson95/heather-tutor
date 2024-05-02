ad01bcfbd3c8f5b6765fda3827010b3efbd53e4c
Brian Nelson <71141646+briannelson95@users.noreply.github.com>
2024-05-02 16:30:51 -0400

    fix not found

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,11 +1,14 @@
 import Navbar from '@/components/Navbar'
 import ChevronLeftIcon from '@/components/icons/ChevronLeftIcon'
+import { client } from '@/sanity/lib/client';
+import { settingsQuery } from '@/sanity/lib/queries';
 import Link from 'next/link'
  
-export default function NotFound() {
+export default async function NotFound() {
+    const data = await client.fetch(settingsQuery);
     return (
         <main>
-            <Navbar />
+            <Navbar image={data.logo} navItems={data.navigation}/>
             <div className='bg-gray-300 min-h-screen px-16 py-6 space-y-2'>
                 <h2 className='text-3xl font-bold'>Page Not Found</h2>
                 <p>Could not find requested resource</p>
@@ -14,6 +17,7 @@ export default function NotFound() {
                     Return Home
                 </Link>
             </div>
+            <Footer image={data.logo} />
         </main>
     )
 }
