f0cff2c34d96b0a46a18ce28435d7274b3142c5a
Brian Nelson <71141646+briannelson95@users.noreply.github.com>
2024-06-15 17:55:06 -0400

    Revert "basic setup"
    This reverts commit 91088283d3fdd329f87dbdf12b6ac7c241e8ed80.

--- app/(public)/page.tsx
+++ app/(public)/page.tsx
@@ -6,10 +6,25 @@ import { client } from "@/sanity/lib/client";
 import { homepageQuery } from "@/sanity/lib/queries";
 
 export default async function Home() {
-  const data = await client.fetch(homepageQuery);
+  const data = await client.fetch(homepageQuery)
+  const hero = data[0].pageBuilder.find((item: any) => item._type === 'hero');
+  const benefits = data[0].pageBuilder.find((item: any) => item._type === 'benefitsSection');
+  const subjects = data[0].pageBuilder.find((item: any) => item._type === 'subjectGallery');
+  const pricing = data[0].pageBuilder.find((item: any) => item._type === 'pricing');
   return (
     <main className="">
-        Hello world
+      <section>
+        <Hero {...hero} />
+      </section>
+      <section id="learn">
+        <TextWithImage {...benefits} />
+      </section>
+      <section>
+        <Gallery {...subjects} />
+      </section>
+      <section id="pricing">
+        <PricingModels {...pricing} />
+      </section>
     </main>
   );
 }
--- sanity/lib/client.ts
+++ sanity/lib/client.ts
@@ -3,9 +3,8 @@ import { createClient } from 'next-sanity'
 import { apiVersion, dataset, projectId, useCdn } from '../env'
 
 export const client = createClient({
-  projectId,
-  dataset,
   apiVersion,
+  dataset,
+  projectId,
   useCdn,
-  perspective: 'published',
 })
